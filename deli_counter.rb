f91c061cb00d78556e9f7ff6b80ef1fe1b635b2c
Jonathan McKnight <themcknightrider@gmail.com>
2020-05-29 19:27:07 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -14,7 +14,7 @@ end
 def take_a_number (deli,name)
   deli.push(name)
   position = deli.index(name)
-  puts "Welcome, #{name}. You are number #{deli.size} in line."
+  puts "Welcome, #{name}. You are number #{deli.length} in line."
 end
 
 def now_serving(deli)
