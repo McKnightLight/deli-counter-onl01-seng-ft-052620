b937227d7b78d549a80a96b6e9bff826369aa47a
Jonathan McKnight <themcknightrider@gmail.com>
2020-05-29 19:23:59 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@ def line(deli)
   if deli.length == 0 
     puts "The line is currently empty."
   else 
-    puts "The line is currently:"
+    message = "The line is currently:"
     
   deli.each_with_index do |name, index| 
     message += " #{index.to_i+1}. #{name}"
